582984b5865e5949f0c0cf3ef16d7ac46a7cdbfa
Felix Teutsch <63754496+FelixTeutsch@users.noreply.github.com>
2022-01-09 21:52:50 +0100

    Updated with a more efficient version

--- image2gray.m
+++ image2gray.m
@@ -1,14 +1,5 @@
 function I_out = image2gray(I_in)
-    % function only works with 3D-uint8 arrays, as other datatypes are not
-    % needed in this program.
-    I_out = zeros(size(I_in,1), size(I_in,2), 'uint8');
-    
-    % take the 3-dimension value (= RGB values) of every 2D element (=
-    % pixels) and calculate the mean value. Place this value in the
-    % corresponding spot (= pixel) in the new array.
-    for i = 1:size(I_in,1)
-        for j = 1:size(I_in,2)
-            I_out(i,j) = mean(I_in(i,j,:));
-        end
-    end
+    % Efficient Matlab version with matrix operations:
+    I_out = (I_in(:,:,1) .* 0.2989 + I_in(:,:,2) .* 0.5870 + I_in(:,:,3) .* 0.1140)./3.0;
+    % Based on: https://de.mathworks.com/help/matlab/ref/rgb2gray.html#buiz8mj-9
 end
