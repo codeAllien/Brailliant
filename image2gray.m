5b855a7c147d8d4d4045b76391160eb7f9afcc58
Marc Putz <marc00.putz@gmail.com>
2022-01-09 02:49:43 +0100

    added comments to image2gray and imagecomplement

--- image2gray.m
+++ image2gray.m
@@ -1,6 +1,11 @@
 function I_out = image2gray(I_in)
+    % function only works with 3D-uint8 arrays, as other datatypes are not
+    % needed in this program.
     I_out = zeros(size(I_in,1), size(I_in,2), 'uint8');
     
+    % take the 3-dimension value (= RGB values) of every 2D element (=
+    % pixels) and calculate the mean value. Place this value in the
+    % corresponding spot (= pixel) in the new array.
     for i = 1:size(I_in,1)
         for j = 1:size(I_in,2)
             I_out(i,j) = mean(I_in(i,j,:));
